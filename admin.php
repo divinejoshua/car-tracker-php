3d333d05a643ae6b300fd096756e983b8724d694
divinejoshua <divineuche399@gmail.com>
2022-03-23 21:06:38 +0000

    Added php to admin

--- admin.php
+++ admin.php
@@ -1,3 +1,76 @@
+<?php
+include('classes/DB.php');
+$_SESSION['message'] = '';
+if (isset($_POST['createaccount'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+
+        if (!DB::query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
+
+                if (strlen($username) >= 3 && strlen($username) <= 32) {
+
+                        if (preg_match('/[a-zA-Z0-9_]+/', $username)) {
+
+                                if (strlen($password) >= 6 && strlen($password) <= 60) {
+
+                                if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                                if (!DB::query('SELECT email FROM users WHERE email=:email', array(':email'=>$email))) {
+
+                                        DB::query('INSERT INTO users VALUES (\'\', :username, :password, :email, \'0\', NOW())', array(':username'=>$username, ':password'=>password_hash($password, PASSWORD_BCRYPT), ':email'=>$email));
+                                        echo "<script>window.open('index.php', '_self')</script>";
+                                         $cstrong = True;
+                        $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+                        $user_id = DB::query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
+                        DB::query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
+
+                        setcookie("SNID", $token, time() + 60 * 60 * 24 * 30, '/', NULL, NULL, TRUE);
+                        setcookie("SNID_", '1', time() + 60 * 60 * 24 * 30, '/', NULL, NULL, TRUE);
+                                } else {
+                                       $_SESSION['message'] = "<font color='#F44336'>*This email account is taken</font>";
+                                }
+                        } else {
+                                        $_SESSION['message'] = "<font color='#F44336'>*Invalid email !!!</font>";
+                                }
+                        } else {
+                               $_SESSION['message'] = "<font color='#F44336'>*Ivalid password !!!</font>";
+                        }
+                        } else {
+                               $_SESSION['message'] = "<font color='#F44336'>*Invalid Username !!!</font>";
+                        }
+                } else {
+                       $_SESSION['message'] = "<font color='#F44336'>*Invalid Username !!!</font>";
+                }
+
+        } else {
+                $_SESSION['message'] = "<font color='#F44336'>*Username is already taken !!!</font>";
+        }
+}
+function isLoggedInUsername(){
+    if (isset($_COOKIE['SNID'])){
+        if (DB::query('SELECT user_id FROM login_tokens WHERE token=:token', array(':token'=>sha1($_COOKIE['SNID'])))){
+$username = DB::query('SELECT username FROM users, login_tokens WHERE token=:token AND users.id = login_tokens.user_id', array(':token'=>sha1($_COOKIE['SNID'])))[0]['username'] ;
+return $username;
+        }
+
+    }
+    return false;
+}
+
+if (isLoggedInUsername()) {
+echo "<script>window.open('index.php', '_self')</script>";
+    
+}
+    else {
+
+    }
+?>
+
+
+
+
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -8,8 +81,8 @@
     <meta name="generator" content="Hugo 0.84.0">
     <title>Add Admins</title>
 
-    <link href="/assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/style.css" rel="stylesheet">
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
    
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
@@ -34,7 +107,7 @@
 
     
     <!-- Custom styles for this template -->
-    <link href="/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
   </head>
   <body>
     
@@ -133,7 +206,7 @@
           
                 <div class="col-md-6">
                   <div class="form-floating">
-                  <input type="text" class="form-control " id="validationServer01" placeholder="first name"  required>
+                  <input type="text" class="form-control " name="firstname" id="validationServer01" placeholder="first name"  required>
                   <label for="validationServer01" class="form-label"> </label>
                   <label for="floatingInput">First Name</label>
                   </div>
@@ -142,7 +215,7 @@
                 <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServer02" class="form-label"></label>
-                  <input type="text" class="form-control " id="validationServer02" placeholder="last name"  required>
+                  <input type="text" class="form-control " name="lastname" id="validationServer02" placeholder="last name"  required>
                   <label for="floatingInput">Last Name</label>
               </div>
                 </div>
@@ -150,7 +223,7 @@
                 <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServerUsername" class="form-label"></label>
-                  <input type="text" class="form-control " id="validationServerUsername" placeholder="username" required>
+                  <input type="text" class="form-control " name="username" id="validationServerUsername" placeholder="username" required>
                   <label for="floatingInput">Username</label>
               </div>
                 </div>
@@ -159,13 +232,13 @@
                 <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServer03" class="form-label"></label>
-                  <input type="text" class="form-control " id="validationServer03" placeholder="city"  required>
+                  <input type="text" class="form-control " name="city" id="validationServer03" placeholder="city"  required>
                   <label for="floatingInput">City</label>
                 </div></div>
       
                 <div class="col-md-6">
                   <div class="form-floating">
-                  <input type="text" class="form-control " id="validationServer04" placeholder="state"  required>
+                  <input type="text" class="form-control " name="state" id="validationServer04" placeholder="state"  required>
                   <label for="floatingInput">State</label>
                   <label for="validationServer04" class="form-label"></label>
                    
@@ -174,7 +247,7 @@
               <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServer05" class="form-label"></label>
-                  <input type="number" class="form-control " id="validationServer05" placeholder="zip" required>
+                  <input type="number" class="form-control " name="zipcode" id="validationServer05" placeholder="zip" required>
                   <label for="floatingInput">Zip Code</label>
                  
               </div></div>
@@ -182,14 +255,14 @@
                 <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServer06" class="form-label"></label>
-                  <input type="email" class="form-control " id="validationServer06" placeholder="mail" required>
+                  <input type="email" class="form-control " name="email" id="validationServer06" placeholder="mail" required>
                   <label for="floatingInput">Email Address</label>
                 </div></div>
                 
                 <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServer07" class="form-label"></label>
-                  <input type="password" class="form-control" id="validationServer07" placeholder="password" required>
+                  <input type="password" class="form-control" name="password" id="validationServer07" placeholder="password" required>
                   <label for="floatingInput">Password</label>
                   </div>
                 </div>
@@ -198,7 +271,7 @@
                 <div class="col-md-6">
                   <div class="form-floating">
                   <label for="validationServer08" class="form-label"></label>
-                  <input type="number" class="form-control" id="validationServer08" placeholder="phone_no" required>
+                  <input type="number" class="form-control" name="phone" id="validationServer08" placeholder="phone_no" required>
                   <label for="floatingInput">Phone number</label>
                   </div>
                 </div>
@@ -238,7 +311,7 @@
 </div>
 
 
-    <script src="/assets/dist/js/bootstrap.bundle.js"></script>
+    <script src="assets/dist/js/bootstrap.bundle.js"></script>
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="/dash.js"></script>
   </body>
